1270cd4753d76571de83fe00276e123fc58710d9
Noda Shimpei <noda.sin@gmail.com>
2016-02-24 19:00:13 +0900

    Hight Performance Browser Networking Chapter 2

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,11 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+execute 'disable_tcp_slow_start_after_idle ' do
+  command 'sysctl net.ipv4.tcp_slow_start_after_idle = 0'
+end
+
+execute 'enable_tcp_window_scaling' do
+  command 'sysctl net.ipv4.tcp_window_scaling = 1'
+end
